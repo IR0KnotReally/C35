d64909dd1d1cde81cfe3299c0d255af60b78c4cd
IR0KnotReally <71969213+IR0KnotReally@users.noreply.github.com>
2020-11-02 14:53:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var canvas, backgroungImg, playerCount, database;
+var gameState = 0;
+var form, player, game;
+
+function setup(){
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+ 
+}
